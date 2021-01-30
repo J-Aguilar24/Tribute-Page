49f783a17b89bea9cf680b72bffb72b407ebc534
José Aguilar <jaam24072000@gmail.com>
2021-01-30 02:06:44 -0600

    añadido peliculas y modificacion python

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/godzila')
+def godzila():
+    return render_template("godzila.html")
+
+@app.route('/kong')
+def kong():
+    return render_template("kong.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
